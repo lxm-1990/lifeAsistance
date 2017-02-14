656e56c0b900bb40343a00e7eae73e8cc84333e8
lxm <1027381389@qq.com>
2017-02-14 20:53:16 +0800

    设置圆形头像

--- app/src/main/java/com/lxm/smartbutler/fragment/UserFragment.java
+++ app/src/main/java/com/lxm/smartbutler/fragment/UserFragment.java
@@ -1,9 +1,16 @@
 package com.lxm.smartbutler.fragment;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -18,7 +25,11 @@ import android.widget.Toast;
 import com.lxm.smartbutler.R;
 import com.lxm.smartbutler.entity.MyUser;
 import com.lxm.smartbutler.ui.LoginActivity;
+import com.lxm.smartbutler.utils.UtilTools;
 import com.lxm.smartbutler.view.CustomDialog;
+
+import java.io.File;
+import java.io.IOException;
 
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
@@ -30,12 +41,18 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class UserFragment extends Fragment implements View.OnClickListener{
 
+    private static final int TAKE_PIC = 0;
+    private static final int CHOOSE_PIC = 1;
+    private static final int CROP_PIC = 3;
+    private static final String IMAGE_NAME = "output_image.png";
+
     private Button btn_logOff,btn_sure_modify;
     private EditText et_name,et_age,et_sex,et_desc;
     private TextView tv_modify;
     private CircleImageView takePic;
     private CustomDialog dialog;
     private Button take_pic,choose_pic,cancel;
+    private Uri take_imageUri;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -67,6 +84,7 @@ public class UserFragment extends Fragment implements View.OnClickListener{
         setEnbled(false);
 
         takePic = (CircleImageView) view.findViewById(R.id.profile_image);
+        UtilTools.getImageViewFromShare(getActivity(),takePic);
         takePic.setOnClickListener(this);
         dialog = new CustomDialog(getActivity(), WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT,R.layout.choose_pic_layout,R.style.theme_dialog, Gravity.BOTTOM);
         dialog.setCancelable(false);
@@ -139,6 +157,101 @@ public class UserFragment extends Fragment implements View.OnClickListener{
                 break;
             case R.id.cancel:
                 dialog.dismiss();
+            case R.id.take_pic:
+                takePicture();
+                break;
+            case R.id.choose_pic:
+                choosePicture();
+                break;
         }
+    }
+
+    private void takePicture() {
+        //getExternalCacheDir关联缓存目录，在6.0以上可以不用动态申请权限
+        File outputImage = new File(getActivity().getExternalCacheDir(),IMAGE_NAME);
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Uri imageUri;
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(getActivity(),"com.lxm.smartbutler.fileprovider",outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        Intent intent_take = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent_take.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(intent_take,TAKE_PIC);
+        dialog.dismiss();
+    }
+    private void choosePicture() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PIC);
+        dialog.dismiss();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != getActivity().RESULT_CANCELED){
+            switch (requestCode) {
+                case TAKE_PIC:{
+                    //遗留问题：android7.0以上系统无法剪裁，开能是uri问题
+                    File outputImage1 = new File(getActivity().getExternalCacheDir(),IMAGE_NAME);
+                    if (Build.VERSION.SDK_INT >= 24) {
+                        take_imageUri = FileProvider.getUriForFile(getActivity(),"com.lxm.smartbutler.fileprovider",outputImage1);
+                    } else {
+                        take_imageUri = Uri.fromFile(outputImage1);
+                    }
+                    startPhotoZoom(take_imageUri);
+                    break;
+                }
+                case CHOOSE_PIC:
+                    startPhotoZoom(data.getData());
+                    break;
+                case CROP_PIC:{
+                    File outputImage = new File(getActivity().getExternalCacheDir(),IMAGE_NAME);
+                    if (outputImage != null) {
+                        outputImage.delete();
+                    }
+                    setImageView(data);
+                    break;
+                }
+
+            }
+        }
+    }
+    private void startPhotoZoom(Uri uri){
+        if (uri == null) {
+            return;
+        }
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setAction("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");// mUri是已经选择的图片Uri
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);// 裁剪框比例
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 320);// 输出图片大小
+        intent.putExtra("outputY", 320);
+        intent.putExtra("return-data",true);
+        startActivityForResult(intent, CROP_PIC);
+    }
+
+    public void setImageView(Intent intent) {
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Bitmap bitmap = bundle.getParcelable("data");
+            takePic.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        UtilTools.setImageViewToShare(getActivity(),takePic);
     }
 }
--- app/src/main/java/com/lxm/smartbutler/utils/UtilTools.java
+++ app/src/main/java/com/lxm/smartbutler/utils/UtilTools.java
@@ -1,8 +1,17 @@
 package com.lxm.smartbutler.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by lxm on 17/2/7.
@@ -12,5 +21,27 @@ public class UtilTools {
     public static void setFont(Context mContext, TextView textView){
         Typeface tf = Typeface.createFromAsset(mContext.getAssets(),"fonts/FONT.TTF");
         textView.setTypeface(tf);
+    }
+
+    public static void setImageViewToShare(Context mContext, ImageView imageView){
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,80,outputStream);
+
+        byte[] array = outputStream.toByteArray();
+        String imageString = new String(Base64.encodeToString(array,Base64.DEFAULT));
+
+        SharePref.setString(mContext,"image",imageString);
+    }
+
+    public static void getImageViewFromShare(Context mContext, ImageView imageView) {
+        String imageString = SharePref.getString(mContext,"image","");
+        if (!TextUtils.isEmpty(imageString)) {
+            byte[] array = Base64.decode(imageString,Base64.DEFAULT);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(array);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
